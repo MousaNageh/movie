9e0664b76172a211b45eb52b92370d0c6f6120c7
MousaNageh <200moussa200@gmail.com>
2022-01-27 22:41:43 +0200

    added filters by title or overview or date

--- movie_app/views.py
+++ movie_app/views.py
@@ -4,14 +4,43 @@ from django.views.generic import TemplateView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.conf import settings
+import os
+import pandas as pd
+import json
+# dataFrame =  pd.DataFrame(dataSet).itertuples()
 class Index(TemplateView):
   template_name = "index.html"
 
 @api_view(["GET"])
 def search(request):
+  title = request.GET.get("title","")
+  overview = request.GET.get("overview","")
+  date = request.GET.get("date","")
+  
 
-  return Response({
-    "title":request.GET.get("title",""),
-    "date":request.GET.get("date",""),
-    "overview":request.GET.get("overview","")
-  },status=status.HTTP_200_OK)
+  results =[]
+  dataSet = pd.read_csv(os.path.join(settings.BASE_DIR, "dataset.csv")) 
+  queryData = None
+  
+  if title and not overview and not date : 
+    queryData = dataSet[dataSet["title"].str.contains(title,case=False,na=False)]
+    results = json.loads(pd.DataFrame(queryData).to_json(orient="records"))
+
+  elif overview and not title and not date: 
+      queryData = dataSet[
+        dataSet['overview'].str.contains(overview,case=False,na=False) 
+        ]
+      results = json.loads(pd.DataFrame(queryData).to_json(orient="records"))
+  
+  elif not title and not overview and date : 
+    print(pd.to_datetime(date))
+    queryData = dataSet[
+        pd.to_datetime(dataSet['release_date']) == pd.to_datetime(date) 
+        ]
+    results = json.loads(pd.DataFrame(queryData).to_json(orient="records"))
+  
+  elif title and overview and date :  
+    pass
+  
+  return Response({"results":results},status=status.HTTP_200_OK)
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,26 @@
 let btn = document.querySelector(".search-btn");
 let movieTitle = document.querySelector("#movie-title");
 let releaseDate = document.querySelector("#release-date");
+let results = document.querySelector(".results");
+let loader = document.querySelector(".loader");
 let overview = document.querySelector("#overview");
 btn.addEventListener("click", function(event) {
     event.preventDefault();
+    loader.classList.remove("loader-hidde");
+
     let movieTitleValue = movieTitle.value;
     let releaseDateValue = releaseDate.value;
     let overviewValue = overview.value;
+    console.log(movieTitleValue,
+        releaseDateValue,
+        overviewValue)
     fetch(`http://127.0.0.1:8000/api/search?title=${movieTitleValue}&date=${releaseDateValue}&overview=${overviewValue}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data)
+            results.textContent = JSON.stringify(data, undefined, 5);
+            loader.classList.add("loader-hidde")
+
+        });
 
 });
